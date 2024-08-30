ecc4f1caedd1de36595ba564404627e1e954e601
adelekecode <femiadeleke601@gmail.com>
2024-08-30 11:34:40 +0100

    docker setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+import path from "path";
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+      },
+    },
+    server: {
+      port: 4020 ? parseInt(process.env.VITE_PORT) : 4020,
+      host: true,
+    },
+  });
+};
